7af592fc8006d6cc6b600aa8187fc2652a76cc03
SheriMarie <140038384+SheriMarie@users.noreply.github.com>
2023-08-03 12:50:18 +1000

    Edited MainMenu Buttons
    Added Play -> New Game -> "opens new scene"
    Added Exit -> Yes -> Dev cod lose pay mode

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -14,23 +14,24 @@ public class Grid : MonoBehaviour
         LessonGrid();
 
         GUI.Box(new Rect(ScreenPlacement(5.5f, 1, 5, 1)), "Mush & Rush");
-        GUI.Label(new Rect(ScreenPlacement(5.5f, 1, 5, 1)), "");
+        //GUI.Label(new Rect(ScreenPlacement(5.5f, 1, 5, 1)), "");
         if (GUI.Button(new Rect(ScreenPlacement(6.5f, 3.75f, 3, 1)), "Play"))
         {
 
         }
 
-        if (GUI.Button(new Rect(ScreenPlacement(6.5f, 5, 3, 1)), "Options"))
+        if (GUI.Button(new Rect(ScreenPlacement(6.5f, 4.9f, 3, 1)), "Options"))
         {
 
         }
 
-        if (GUI.Button(new Rect(ScreenPlacement(6.5f, 6, 3, 1)), "Exit"))
+        if (GUI.Button(new Rect(ScreenPlacement(6.5f, 6.10f, 3, 1)), "Exit"))
         {
 
         }
 
     }
+
     //GUI element that is a type of box - box is = to Canvas Image
     //New Rect = Canvas Rect Transform
     //new Rect (StartPositionX, StartPositionY, SizeX, SizeY), "Content" ie an image or a sprite or text ect.)
@@ -56,7 +57,7 @@ public class Grid : MonoBehaviour
     }
 
     //Function - has a return type (kinda like a calculator? It has to give you an answer based on input you give it.)
-    public Rect ScreenPlacement(float startPositionX, float startPositionY, float sizeX, float sizeY)
+    public static Rect ScreenPlacement(float startPositionX, float startPositionY, float sizeX, float sizeY)
     {
         Rect placement = new Rect(startPositionX*Screen.width / 16, startPositionY * Screen.height/9, sizeX * Screen.width / 16, sizeY * Screen.height/9);
         return placement;
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,152 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables and Data
+    public enum MenuState
     {
-        
+        MainMenu,
+        PlayPanel,
+        OptionsPanel,
+        AreYouSurePanel
+
+    }
+    [Header("Menu States")]
+
+    public MenuState menuState = MenuState.MainMenu;
+    #endregion
+
+    #region GUI Panels
+    private void OnGUI()
+    {
+        switch (menuState)
+        {
+            case MenuState.MainMenu:
+                {
+                    MenuPanel();
+                }
+                break;
+            case MenuState.PlayPanel:
+                {
+                    PlayPanel();
+                }
+                break;
+            case MenuState.OptionsPanel:
+                {
+                    OptionsPanel();
+                }
+                break;
+            case MenuState.AreYouSurePanel:
+                {
+                    AreyouSurePanel();
+                }
+                break;
+            default:
+                menuState = MenuState.MainMenu;
+                break;
+        }
+
+    }
+    void MenuPanel()
+    {
+        GUI.Box(new Rect(Grid.ScreenPlacement(0, 0, 16, 9)), "Main Menu");
+        GUI.Box(new Rect(Grid.ScreenPlacement(4, 0.25f, 8, 2)), "Mush & Rush");
+        GUI.Box(new Rect(Grid.ScreenPlacement(5, 2.5f, 6, 6.25f)), "Button Box");
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 3.25f, 4, 0.75f)), "Play"))
+        {
+            menuState = MenuState.PlayPanel;
+        }
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 5.25f, 4, 0.75f)), "Options"))
+        {
+            menuState = MenuState.OptionsPanel;
+        }
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 7.25f, 4, 0.75f)), "Exit"))
+        {
+            menuState = MenuState.AreYouSurePanel;
+        }
+    }
+    void PlayPanel()
+    {
+
+        GUI.Box(new Rect(Grid.ScreenPlacement(0, 0, 16, 9)), "PlayPanel");
+        GUI.Box(new Rect(Grid.ScreenPlacement(4, 0.5f, 8, 8)), "Button Box");
+        GUI.Box(new Rect(Grid.ScreenPlacement(5, 1.2f, 6, 1.5f)), "PlayPanel");
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 3.5f, 4, 0.75f)), "Continue"))
+        {
+            Debug.Log("In future we will load our lasted played saved game");
+        }
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 4.75f, 4, 0.75f)), "New Game"))
+        {
+            ChangeSceneByIndexValue(1);
+        }
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 6, 4, 0.75f)), "Load"))
+        {
+            Debug.Log("In future we will load from a lst of saved games");
+        }
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 7.25f, 4, 0.75f)), "Back"))
+        {
+            menuState = MenuState.MainMenu;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OptionsPanel()
     {
-        
+        GUI.Box(new Rect(Grid.ScreenPlacement(0, 0, 16, 9)), "OptionsPanel");
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(6, 7.25f, 4, 0.75f)), "Back"))
+        {
+            menuState = MenuState.MainMenu;
+        }
     }
+    void AreyouSurePanel()
+    {
+        GUI.Box(new Rect(Grid.ScreenPlacement(0, 0, 16, 9)), "AreYouSurePanel");
+        GUI.Box(new Rect(Grid.ScreenPlacement(5, 2.5f, 6, 5f)), "AreYouSurePanel");
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(5, 4.75f, 2.75f, 0.75f)), "Yes"))
+        {
+            ExitToDeskTop();
+        }
+
+        if (GUI.Button(new Rect(Grid.ScreenPlacement(8.25f, 4.75f, 2.75f, 0.75f)), "No"))
+        {
+            menuState = MenuState.MainMenu;
+        }
+    }
+    #endregion
+
+    #region Scene Management
+
+    void ChangeSceneByName(string nameOfScene)
+    {
+        SceneManager.LoadScene(nameOfScene);
+    }
+
+
+    void ChangeSceneByIndexValue(int indexValueofScene)
+    {
+        SceneManager.LoadScene(indexValueofScene);
+    }
+
+    void ExitToDeskTop()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+    #endregion
 }
