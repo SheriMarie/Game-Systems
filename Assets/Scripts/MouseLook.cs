e3a02602d8e8bba769eaf928a3bf187b2070e186
SheriMarie <140038384+SheriMarie@users.noreply.github.com>
2023-08-03 12:56:49 +1000

    Added Base Movement and Mouse Lok Script to Game Scene

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+//this script can be found in the Component section under the option PRG/Mouse Look
+public class MouseLook : MonoBehaviour
+{
+    /*
+     enums are what we call state value variables 
+     they are comma separated lists of identifiers
+     we can use them to create Types or Categorys
+         */
+    #region RotationalAxis
+    //Create a public enum called RotationalAxis
+    
+    #endregion
+    #region Variables
+    //[Header("Rotation")]
+    //create a public link to the rotational axis called axis and set a defualt axis
+    //[Header("Sensitivity")]
+    //public floats for our x and y sensitivity
+    //[Range(0, 100)]
+    //[Header("Y Rotation Clamp")]
+    //max and min Y rotation
+    //we will have to invert our mouse position later to calculate our mouse look correctly
+    //float for rotation Y
+    #endregion
+    #region Start
+        //Lock Cursor to middle of screen
+        //Hide Cursor from view
+        //if our game object has a rigidbody attached to it
+            //set the rigidbodys freezRotaion to true
+    #endregion
+    #region Update
+        #region Mouse X
+        //else if we are rotating on the X
+            //transform the rotation on our game objects Y by our Mouse input Mouse X times X sensitivity
+            //x                y                          z
+        #endregion
+        #region Mouse Y
+        //else we are only rotation on the Y
+            //our rotation Y is pulse equals  our mouse input for Mouse Y times Y sensitivity
+            //the rotation Y is clamped using Mathf and we are clamping the y rotation to the Y min and Y max
+            //transform our local position to the nex vector3 rotaion - y rotaion on the x axis and local euler angle Y on the y axis
+    #endregion
+    #endregion
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+//this script can be found in the Component section under the option Intro PRG/Character Movement
+//This script requires the component Character controller to be attached to the Game Object
+public class Movement : MonoBehaviour 
+{
+    #region Extra Study
+    //Input Manager(https://docs.unity3d.com/Manual/class-InputManager.html)
+    //Input(https://docs.unity3d.com/ScriptReference/Input.html)
+    //CharacterController allows you to move the character kinda like Rigidbody (https://docs.unity3d.com/ScriptReference/
+    #endregion
+    #region Variables
+    //[Header("Character")]
+    //vector3 called moveDir //we will use this to apply movement in worldspace
+    //Character controller called _charC CharacterController.html) 
+    //[Header("Character Speeds")]
+    //public float variables jumpSpeed 8 & speed 5 & gravity 20 
+    #endregion
+    #region Start  
+        //_charc is set to the Character controller on this GameObject
+     #endregion
+    #region Update
+        //if our character is grounded
+            //set moveDir to the inputs direction
+            //moveDir's forward is changed from global z (forward) to the Game Objects local Z (forward)//allows us to move where player is facing
+            //moveDir is multiplied by speed so we move at a decent pace
+            //if the input button for jump is pressed then           
+                //our moveDir.y is equal to our jump speed
+        //regardless of if we are grounded or not the players moveDir.y is always affected by gravity timesed my time.deltaTime to normalize it
+        //we then tell the character Controller that it is moving in a direction multiplied Time.deltaTime    
+    #endregion
+}
+
+
+
+
+
+
+
+
+
+
